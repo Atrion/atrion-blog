883b8e9db4e7e6379ad0fb994c9153c9546567e0
Atrion Darnay <Atrion@gmail.com>
2025-04-26 00:46:19 -0300

    fixing default

--- assets/js/darkmode.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-  const toggle = document.createElement('button');
-  toggle.textContent = 'Toggle Dark Mode';
-  toggle.onclick = () => document.body.classList.toggle('dark');
-  document.body.insertBefore(toggle, document.body.firstChild);
-});
